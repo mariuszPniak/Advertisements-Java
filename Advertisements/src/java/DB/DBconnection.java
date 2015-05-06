4c488ad3b1e86f765ab844a1f6d1c832fcec5ace
Mariusz Pniak <mariusz899@gmail.com>
2015-05-06 16:15:59 +0200

    commit

--- Advertisements/src/java/DB/DBconnection.java
+++ Advertisements/src/java/DB/DBconnection.java
@@ -17,9 +17,9 @@ public class DBconnection {
             Logger.getLogger(DBconnection.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        String url="jdbc:postgresql://localhost:5432/advertisements";
-        String user="postgres";
-        String password="postgres";
+        String url="jdbc:postgresql://178.19.104.18:5432/webtarsl_ads";
+        String user="webtarsl_webtar";
+        String password="webtar";
         
         Connection con=null;
         
